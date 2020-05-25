eafe4f0f2574a0bdf3d4f8a7654f74ee2d2febb6
MarkKaarma <mark.kaarma@gmail.com>
2020-05-25 17:20:38 +0300

    Added picture into Form

--- Triangle_Kaarma/Form1.Designer.cs
+++ Triangle_Kaarma/Form1.Designer.cs
@@ -30,6 +30,8 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -37,7 +39,7 @@
             this.button1.BackColor = System.Drawing.Color.White;
             this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Cyan;
             this.button1.FlatAppearance.BorderSize = 10;
-            this.button1.Location = new System.Drawing.Point(484, 74);
+            this.button1.Location = new System.Drawing.Point(678, 269);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(185, 95);
             this.button1.TabIndex = 0;
@@ -45,16 +47,27 @@
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Triangle_Kaarma.Properties.Resources.PF_ETR6;
+            this.pictureBox1.Location = new System.Drawing.Point(-23, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(685, 636);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(824, 361);
+            this.ClientSize = new System.Drawing.Size(930, 669);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Это Каарма? Тебе треугольник передает поклон";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -62,6 +75,7 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
