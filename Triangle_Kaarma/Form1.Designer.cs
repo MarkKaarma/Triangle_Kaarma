caa820e8f61a20df3bc75a15d35855e6f14db016
MarkKaarma <mark.kaarma@gmail.com>
2020-06-15 11:06:54 +0300

    Code fix

--- Triangle_Kaarma/Form1.Designer.cs
+++ Triangle_Kaarma/Form1.Designer.cs
@@ -31,6 +31,10 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -39,7 +43,7 @@
             this.button1.BackColor = System.Drawing.Color.White;
             this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Cyan;
             this.button1.FlatAppearance.BorderSize = 10;
-            this.button1.Location = new System.Drawing.Point(678, 269);
+            this.button1.Location = new System.Drawing.Point(703, 43);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(185, 95);
             this.button1.TabIndex = 0;
@@ -50,23 +54,38 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::Triangle_Kaarma.Properties.Resources.PF_ETR6;
-            this.pictureBox1.Location = new System.Drawing.Point(-23, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(1, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(685, 636);
+            this.pictureBox1.Size = new System.Drawing.Size(638, 621);
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(691, 166);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(197, 203);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(930, 669);
+            this.Controls.Add(this.listView1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Это Каарма? Тебе треугольник передает поклон";
+            this.Text = "Это Каарма? Вам треугольник передает поклон";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
@@ -76,6 +95,10 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
     }
 }
 
--- Triangle_Kaarma/Triangle.cs
+++ Triangle_Kaarma/Triangle.cs
@@ -13,60 +13,57 @@ namespace Triangle_Kaarma
         public double c; // Третья сторона 
         public double h; // Высота *не знаю, будет ли работать, так уж просто так добавил ради ЭКСПЕРЕМЕНТА!
 
-        public Triangle(double A, double B, double C) // Обычный треугольник
+        public Triangle(double A, double B, double C) // Обычный треугольник 
         {
             a = A;
             b = B;
             c = C;
         }
-        public Triangle(double A, double B, double C, double H) // Конструктор, который позволит сделать произвольную высоту и стороны 
+        public Triangle(double A, double B, double C, double H) //  Конструктор, который позволит вычислить высоту и стороны  
         {
             a = A;
-            b = B;  // Создаем с заданными длинами сторон и высоты согласно заданию 
-            с = C;
+            b = B; // Создаем с заданными длинами сторон и высоту согласно заданию 
+            c = C;
             h = H;
         }
         public Triangle() // Конструктор, создаст без указания переменов
         {
         }
-        public string outputA() // Выводим сторону а
+        public string outputA() // Выводим сторону А
         {
             return Convert.ToString(a); // а - Ссылка на внутреннее поле объекта класса 
         }
-        public string outputB() // Выводим сторону b
+        public string outputB() // Выводим сторону B
         {
             return Convert.ToString(b); // b - Ссылка на внутреннее поле объекта класса 
         }
-        public string outputC() // Выводим сторону c
+        public string outputC() // Выводим сторону C
         {
             return Convert.ToString(c); // c - Ссылка на внутреннее поле объекта класса 
         }
-
-        }
-        public double Perimeter() // Сумма всех сторон типа double
+        public double Perimeter() // // Сумма всех сторон типа double
         {
-            double p = 0;
+            double p = 0; 
             p = a + b + c; // Формула периметра
             return p; // Выводим
         }
-        public double Surface() // Альтернативный способ вычисление периметра и вычисление площади
+        public double Surface() // Альтернативный способ вычисление периметра и вычисление площади 
         {
             double s = 0;
             double p = 0;
-            p = (a + b + c) / 2; // Формула
-            s = Math.Sqrt((p * (p - a) * (p - b) - (p - c))); // Формула
+            p = (a + b + c) / 2; // Формула 
+            s = Math.Floor(Math.Sqrt((p * (p - a) * (p - b) * (p - c)))); // Формула
             return s; // Выводим площадь 
         }
-        // В связи с тем, что я не сделал свои конструкторы и не особо понял, как сделать их так уж нашел в Интернете некоторые примеры и решил их взять
-        public double Halfperimeter() // Для вычисление высоты мы разделяем на половину периметра
+        public double HalfPerimeter() // Для вычисление высоты мы разделяем на половину периметра
         {
             return Perimeter() / 2; // Формула
         }
-        public double Height () // Вычисление высоты 
+        public double Height() // Вычисление высоты 
         {
-            double p = Halfperimeter(); 
-            double h = 2 * Math.Sqrt(p * (p - a) * (p - b) * (p - c)) / a; // Формула 
-            return h; // Выводим высоту
+            double p = HalfPerimeter();
+            double h = Math.Floor(2 * Math.Sqrt(p * (p - a) * (p - b) * (p - c)) / a);
+            return h;
         }
         public double GetSetA // Свойство позволяющее установить либо изменить сторону
         {
@@ -82,7 +79,7 @@ namespace Triangle_Kaarma
             set
             { b = value; }
         }
-        public double GetSetC // Свойство позволяющее установить либо изменить сторону
+        public double GetSetC // Свойство позволяющее установить сторону
         {
             get
             { return c; }
@@ -90,14 +87,13 @@ namespace Triangle_Kaarma
             { c = value; }
         }
 
-        }
         public bool ExistTriangle // Свойство позволяющее установить, существует ли треугольник с заданными сторонами
         {
             get
             {
                 if ((a > b + c) && (b > a + c) && (c > a + b)) // Сумма двух сторон должна быть больше третьей
-                return false;
-                else return true;
+                    return true;
+                else return false;
             }
         }
     }
